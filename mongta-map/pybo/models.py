d536ef328809836e5d110aadf4cc248dedb56cf9
hyunmin <choihm9903@naver.com>
2022-01-12 16:12:54 +0900

    Update mongta-map

--- mongta-map/pybo/models.py
+++ mongta-map/pybo/models.py
@@ -3,13 +3,16 @@ from django.contrib.auth.models import User
 
 class Question(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_question')
-    subject = models.CharField(max_length=200)                      # 데이터명
-    content = models.TextField()                                    # 데이터카테고리
+    subject = models.CharField(max_length=200)                      # 데이터명 - 0
+    category = models.CharField(max_length=500, blank=True)         # 카테고리 - 1
+    agency = models.CharField(max_length=500, blank=True)           # 기관 - 2
+    update_date = models.CharField(max_length=500, blank=True)      # 업데이트 주기 - 4
+    content = models.TextField()                                    # 설명 - 10
+    url = models.CharField(max_length=500, blank=True)              # 데이터 url - 14
 
     create_date = models.DateTimeField()                            # 등록일
     modify_date = models.DateTimeField(null=True, blank=True)       # 수정일
 
-    url = models.CharField(max_length=500, blank=True)              # 데이터url
 
     voter = models.ManyToManyField(User, related_name='voter_question')
 
--- mongta-map/pybo/views/db_views.py
+++ mongta-map/pybo/views/db_views.py
@@ -7,21 +7,21 @@ from pybo.models import Question
 from django.utils import timezone
 from django.contrib.auth.models import User
 
-def save(subject, content, url):
+def save(subject, category, agency, update_date, content, url):
     user = User.objects.get(id=2)
-    d = Question(author=user, subject=subject, content=content, create_date=timezone.now(), url=url)
+    d = Question(author=user, subject=subject, category=category, agency=agency, update_date=update_date, content=content, create_date=timezone.now(), url=url)
     d.save()
 
 def update(self):
     BASE_DIR = Path(__file__).resolve().parent.parent
     data = list()
-    f = open(str(BASE_DIR)+"\DB.csv", 'r')
+    f = open(str(BASE_DIR)+"\DB.csv", 'r', encoding='utf-8-sig')
     rea = csv.reader(f)
     for row in rea:
         data.append(row)
 
     for d in data:
-        save(d[0], d[1], d[9])
+        save(d[0], d[1], d[2], d[4], d[10], d[14])
 
     return HttpResponse("업데이트 완료")
 
