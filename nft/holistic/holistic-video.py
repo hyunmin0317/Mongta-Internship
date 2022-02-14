4c540993ba1ed2e47b58b303071ad054c30bc670
hyunmin0317 <choihm9903@naver.com>
2022-02-14 14:17:23 +0900

    feat: holistic

--- nft/holistic/holistic-video.py
+++ nft/holistic/holistic-video.py
@@ -1,12 +1,13 @@
 import cv2
 import mediapipe as mp
+import numpy as np
 
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_holistic = mp.solutions.holistic
 
 # For webcam input:
-cap = cv2.VideoCapture('./test4.mp4')
+cap = cv2.VideoCapture('./test3.mp4')
 
 # 재생할 파일의 넓이와 높이
 width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
@@ -15,7 +16,7 @@ height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
 print("재생할 파일 넓이, 높이 : %d, %d"%(width, height))
 
 fourcc = cv2.VideoWriter_fourcc(*'DIVX')
-out = cv2.VideoWriter('output.avi', fourcc, 30.0, (int(width), int(height)))
+out = cv2.VideoWriter('nft.avi', fourcc, 30.0, (int(width), int(height)))
 
 # holistic
 with mp_holistic.Holistic(
@@ -37,24 +38,28 @@ with mp_holistic.Holistic(
     # Draw landmark annotation on the image.
     image.flags.writeable = True
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+    image_holistic = np.zeros_like(image)
+
+
     mp_drawing.draw_landmarks(
-        image,
+        image_holistic,
         results.face_landmarks,
         mp_holistic.FACEMESH_CONTOURS,
         landmark_drawing_spec=None,
         connection_drawing_spec=mp_drawing_styles
         .get_default_face_mesh_contours_style())
     mp_drawing.draw_landmarks(
-        image,
+        image_holistic,
         results.pose_landmarks,
         mp_holistic.POSE_CONNECTIONS,
         landmark_drawing_spec=mp_drawing_styles
         .get_default_pose_landmarks_style())
     # Flip the image horizontally for a selfie-view display.
 
-    print(results.pose_landmarks.landmark[mp_holistic.PoseLandmark.NOSE])
-    cv2.imshow('MediaPipe Holistic', image)
-    out.write(image)
+    # print(results.pose_landmarks.landmark[mp_holistic.PoseLandmark.NOSE])
+    cv2.imshow('MediaPipe Holistic', image_holistic)
+    out.write(image_holistic)
     if cv2.waitKey(5) & 0xFF == 27:
       break
 cap.release()
